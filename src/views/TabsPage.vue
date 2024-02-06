f458ef07953ee65ebc9bae4663a52a1b13ed31a1
Trevor_143 <trevorarapu2@gmail.com>
2024-02-06 12:51:18 +0300

    initialized app

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -17,12 +17,18 @@
           <ion-icon aria-hidden="true" :icon="square" />
           <ion-label>Tab 3</ion-label>
         </ion-tab-button>
+        
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-label>Tab 3</ion-label>
+        </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
+
 </script>
